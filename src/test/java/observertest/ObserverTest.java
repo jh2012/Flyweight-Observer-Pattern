f124b63fa6383fff16aaa7f0f2a6e19397c2dcb7
jh2012 <35670499+jh2012@users.noreply.github.com>
2018-04-26 00:12:26 -0500

    Update ObserverTest.java

--- src/test/java/observertest/ObserverTest.java
+++ src/test/java/observertest/ObserverTest.java
@@ -80,30 +80,4 @@ public class ObserverTest {
     assertEquals(1, ((FlyweightObserver) observer2).getWhatObserverKnows());
     assertEquals(1, ((FlyweightObserver) observer3).getWhatObserverKnows());
   }
-  
-  @Test
-  public void testNotifyObserver2() throws CloneNotSupportedException {
-
-    FlyweightSubjectFactory fwSFactory = new FlyweightSubjectFactory();
-
-    FlyweightSubject subject = (FlyweightSubject) fwSFactory.getSubject("ObserverTest", "testSubject");
-
-    UnSharedFlyweightObserver observer1 = new UnSharedFlyweightObserver(subject);
-    UnSharedFlyweightObserver observer2 = new UnSharedFlyweightObserver(subject);
-    UnSharedFlyweightObserver observer3 = new UnSharedFlyweightObserver(subject);
-    subject.attach(observer1);
-    subject.attach(observer2);
-    subject.attach(observer3);
-    subject.setState(1);
-    subject.notifyObservers();
-
-    subject.dettach(observer1);
-    subject.setState(0);
-    subject.notifyObservers();
-
-    // observer is detached so changes are not propageted
-    assertEquals(1, ((FlyweightObserver) observer1).getWhatObserverKnows());
-    assertEquals(0, ((FlyweightObserver) observer2).getWhatObserverKnows());
-    assertEquals(0, ((FlyweightObserver) observer3).getWhatObserverKnows());
-  }
 }
