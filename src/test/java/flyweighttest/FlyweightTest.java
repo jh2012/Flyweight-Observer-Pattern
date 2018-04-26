3aa38a36378b835704197073a6593eb601cf8015
jh2012 <35670499+jh2012@users.noreply.github.com>
2018-04-25 23:16:31 -0500

    Update FlyweightTest.java

--- src/test/java/flyweighttest/FlyweightTest.java
+++ src/test/java/flyweighttest/FlyweightTest.java
@@ -13,27 +13,17 @@ import org.junit.jupiter.api.Test;
 public class FlyweightTest {
 
   @Test
-  public void testFlyweight() {
+  public void testFlyweightShared() {
 
     // Create 3 FlyweightSubject instances.
     SharedFlyweightSubject sfS1 = (SharedFlyweightSubject) FlyweightSubjectFactory.getSubject("key1", "value1");
     SharedFlyweightSubject sfS2 = (SharedFlyweightSubject) FlyweightSubjectFactory.getSubject("key2", "value2");
     SharedFlyweightSubject sfS3 = (SharedFlyweightSubject) FlyweightSubjectFactory.getSubject("key1", "value3");
 
-    // Create 3 UnSharedFlyweight instances. 
-    UnSharedFlyweightSubject usfS1 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey1","value1");
-    UnSharedFlyweightSubject usfS2 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey2","value2");
-    UnSharedFlyweightSubject usfS3 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey1","value3");
-
     // 3 shared flyweight object equivalency tests
     assertNotEquals(sfS1, sfS2);
     assertEquals(sfS1, sfS3);
     assertNotEquals(sfS2, sfS3);
-
-    // 3 unshared flyweight object non-equivalency tests
-    assertNotEquals(usfS1, usfS2);
-    assertNotEquals(usfS1, usfS3);
-    assertNotEquals(usfS2, usfS3);
 
    // 3 shared flyweight tests assessing the values for the shared (instrinsic) state.
 
@@ -48,6 +38,20 @@ public class FlyweightTest {
     if (sfS3 instanceof com.hundredwordsgof.flyweightobserver.FlyweightSubject) {
       assertEquals("value1", sfS3.getIntrinsicState());
     }
+  }
+  
+  @Test
+  public void testFlyweightUnShared() {
+
+    // Create 3 UnSharedFlyweight instances. 
+    UnSharedFlyweightSubject usfS1 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey1","value1");
+    UnSharedFlyweightSubject usfS2 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey2","value2");
+    UnSharedFlyweightSubject usfS3 = (UnSharedFlyweightSubject) FlyweightSubjectFactory.getSubject("unsharedKey1","value3");
+
+    // 3 unshared flyweight object non-equivalency tests
+    assertNotEquals(usfS1, usfS2);
+    assertNotEquals(usfS1, usfS3);
+    assertNotEquals(usfS2, usfS3);
 
     // 3 unshared flyweight tests assessing the values of their states.
 
